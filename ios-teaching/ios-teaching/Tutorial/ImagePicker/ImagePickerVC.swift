780430c135ed38e5391650aa3d3f6c3844cb9e4a
ha_linh <haduclinhbkit@gmail.com>
2018-05-26 11:09:45 +0700

    add Image picker view controller

---
+++ ios-teaching/ios-teaching/Tutorial/ImagePicker/ImagePickerVC.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePickerVC.swift
+//  ios-teaching
+//
+//  Created by Ha Linh on 5/25/18.
+//  Copyright © 2018 Ha Linh. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerVC: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var floatButton: UIButton!
+    @IBOutlet weak var ButtonToBottomContraint: NSLayoutConstraint!
+    var newContraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+//        self.view.removeConstraint(ButtonToBottomContraint)
+//        newContraint = NSLayoutConstraint(item: self.view, attribute: .bottom, relatedBy: .equal, toItem: floatButton, attribute: .bottom, multiplier: 1, constant: 100)
+//        self.view.addConstraint(newContraint)
+    }
+    
+    var currentOffset: CGFloat = 0
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if offset > currentOffset {
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                self.ButtonToBottomContraint.constant = 0
+                self.view.layoutIfNeeded()
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.ButtonToBottomContraint.constant = 100
+                self.view.layoutIfNeeded()
+            })
+        }
+        currentOffset = offset
+    }
+}
